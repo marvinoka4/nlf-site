8fb9e4755d882c2410d4b9518ecce2c34964a924
Marvin Okafor <marvinoka4@gmail.com>
2023-11-12 16:56:29 +0000

    update: office address

--- page-templates/contact-us.php
+++ page-templates/contact-us.php
@@ -76,8 +76,6 @@ get_header(); ?>
                 <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3464.7427695753604!2d-95.57349038836972!3d29.727209533426834!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x8640dcd05e72e167%3A0x148e2eb0a0d529c1!2s11011%20Richmond%20Ave.%2C%20Houston%2C%20TX%2077042%2C%20USA!5e0!3m2!1sen!2suk!4v1699808023326!5m2!1sen!2suk" width="100%" height="450" style="border:0; border-radius: 2px;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
             </div>
 
-          <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3464.7427695753604!2d-95.57349038836972!3d29.727209533426834!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x8640dcd05e72e167%3A0x148e2eb0a0d529c1!2s11011%20Richmond%20Ave.%2C%20Houston%2C%20TX%2077042%2C%20USA!5e0!3m2!1sen!2suk!4v1699808023326!5m2!1sen!2suk" width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
-
 
         </div>
 
